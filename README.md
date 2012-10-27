bbe001fa680db7400ece62bed301425bb6b9328e
NatashaMurashev <nmurashev@gmail.com>
2012-10-27 09:20:39 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+For 2013, my goal is to payoff all my student loans.
+
+This app will help me and others with similar goals keep track of their progress.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Payoffyear::Application.config.secret_token = '496949fb280ab66205a72a4cf6f4635cec26ef92f2b665b43ef52d35efe8ae889ca3bebb0e83a4b60c656bef2de4c849fb0d1f378343a87fef8d6b913293f04a'
